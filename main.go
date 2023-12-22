dfe336e11f56816309f59fbeb8465945bfbda99d
Haseeb Ali <h45.busy@gmail.com>
2023-12-22 17:20:32 +0500

    Seed Data

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 )
 
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	account, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.CreateAccount(account); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("new account ==> ", account.Number)
+
+	return account
+}
+
+func seedData(s Storage) {
+	seedAccount(s, "Ali", "Raza", "password@123")
+}
+
 func main() {
+	seed := flag.Bool("seed", false, "Seed the database")
+	flag.Parse()
+
 	store, err := NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
@@ -12,6 +36,11 @@ func main() {
 
 	if err := store.Init(); err != nil {
 		log.Fatal(err)
+	}
+
+	if *seed {
+		fmt.Println("Seeding database...")
+		seedData(store)
 	}
 
 	server := NewAPIServer(":3000", store)
