f7c96a0cdb25cef2c318eca168170902d8109a04
Haseeb Ali <h45.busy@gmail.com>
2023-12-22 17:21:16 +0500

    Add Login Response & Request Types

--- types.go
+++ types.go
@@ -3,7 +3,20 @@ package main
 import (
 	"math/rand"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+type LoginResponse struct {
+	Number    int64  `json:"number"`
+	Token     string `json:"token"`
+	ExpiredAt string `json:"expired_at"`
+}
+
+type LoginRequest struct {
+	Number   int64  `json:"number"`
+	Password string `json:"password"`
+}
 
 type TransferReqest struct {
 	ToAccount int64 `json:"to_account"`
@@ -13,24 +26,36 @@ type TransferReqest struct {
 type CreateAccountRequest struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
+	Password  string `json:"password"`
 }
 
 type Account struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Number    int64     `json:"number"`
-	Balance   int64     `json:"balance"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID                int       `json:"id"`
+	FirstName         string    `json:"first_name"`
+	LastName          string    `json:"last_name"`
+	Number            int64     `json:"number"`
+	EncryptedPassword string    `json:"-"`
+	Balance           int64     `json:"balance"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
 }
 
-func NewAccount(firstName, lastName string) *Account {
-	return &Account{
-		FirstName: firstName,
-		LastName:  lastName,
-		Number:    int64(rand.Intn(1000000000)),
-		CreatedAt: time.Now().UTC(),
-		UpdatedAt: time.Now().UTC(),
+func (account *Account) ValidPassword(password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(account.EncryptedPassword), []byte(password)) == nil
+}
+
+func NewAccount(firstName, lastName, password string) (*Account, error) {
+	encrypted, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
 	}
+
+	return &Account{
+		FirstName:         firstName,
+		LastName:          lastName,
+		Number:            int64(rand.Intn(1000000000)),
+		EncryptedPassword: string(encrypted),
+		CreatedAt:         time.Now().UTC(),
+		UpdatedAt:         time.Now().UTC(),
+	}, nil
 }
